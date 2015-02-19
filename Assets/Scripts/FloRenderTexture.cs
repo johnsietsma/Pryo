957e7fc3ce9a1cc35673ec22bc980e874b35e812
John Sietsma <john.sietsma@gmail.com>
2015-02-19 16:58:31 +1100

    In process render texture work.

---
+++ Assets/Scripts/FloRenderTexture.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloRenderTexture : MonoBehaviour {
+
+    [SerializeField]
+    private Texture2D initTexture;
+
+    [SerializeField]
+    private Material renderMaterial;
+
+    [SerializeField]
+    private Material displayMaterial;
+
+    private RenderTexture renderTexture;
+
+    void Start()
+    {
+        /*if(!SystemInfo.SupportsRenderTextureFormat(RenderTextureFormat.ARGBFloat))
+        {
+            Debug.LogError("Float render textures are not supported.");
+            enabled = false;
+            return;
+        }*/
+        renderTexture = new RenderTexture(256, 256, 0, RenderTextureFormat.ARGBFloat);
+        //Graphics.Blit( initTexture, renderTexture );
+        displayMaterial.mainTexture = renderTexture;
+    }
+
+    void OnPostRender()
+    {
+        Graphics.Blit(initTexture, renderTexture, renderMaterial);
+    }
+}
---
+++ Assets/Shaders/DisplayVectorField.shader
@@ -0,0 +1,26 @@
+﻿Shader "Custom/Vector Display" {
+	Properties{
+		_MainTex("Base (RGB)", 2D) = "white" {}
+	}
+	SubShader{
+			Pass{
+				Tags{ "RenderType" = "Opaque" }
+				LOD 200
+
+				CGPROGRAM
+				#include "flo.cginc"
+				#include "UnityCG.cginc"
+
+				#pragma vertex vert_img
+				#pragma fragment frag
+
+				sampler2D _MainTex;
+
+				fixed4 frag(v2f_img i) : COLOR{
+					return tex2D(_MainTex, i.uv);
+				}
+				ENDCG
+			}
+		}
+		FallBack "Diffuse"
+}
